ba9d091c650caf99e338b4e0272c9aa4b5785872
LandonHarter <landonharter@outlook.com>
2024-10-25 22:09:52 -0400

    basic imgui support

--- src/main/java/org/landon/core/Window.java
+++ src/main/java/org/landon/core/Window.java
@@ -1,5 +1,6 @@
 package org.landon.core;
 
+import imgui.ImGui;
 import org.joml.Vector2f;
 import org.landon.input.Input;
 import org.lwjgl.glfw.GLFW;
@@ -14,9 +15,11 @@ public class Window {
 
     private int width, height;
     private String title;
+    private boolean minimized;
 
     private long window;
     private GLFWWindowSizeCallback windowSize;
+
 
     public Window(int width, int height, String title) {
         this.width = width;
@@ -53,6 +56,7 @@ public class Window {
                 height = h;
 
                 GL11.glViewport(0, 0, width, height);
+                ImGui.getIO().setDisplaySize(width, height);
             }
         };
 
@@ -67,14 +71,30 @@ public class Window {
         GLFW.glfwSwapInterval(0);
 
         GL11.glViewport(0, 0, width, height);
+        Gui.initialize(window);
+    }
+
+    public void destroy() {
+        windowSize.free();
+
+
+        GLFW.glfwDestroyWindow(window);
+        GLFW.glfwTerminate();
     }
 
     public void startFrame() {
         Time.startFrame();
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+        Gui.startFrame();
+        ImGui.newFrame();
+        ImGui.getIO().setFramerate(60);
     }
 
     public void endFrame() {
+        ImGui.render();
+        Gui.endFrame();
+
         Input.resetDelta();
         GLFW.glfwPollEvents();
         GLFW.glfwSwapBuffers(window);
